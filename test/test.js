a8318b3895ca3c005f30cce980b8255c46dbf522
Jan Bölsche <regular.gonzales@googlemail.com>
2014-08-06 14:32:22 +0200

    added gloss and background pattern

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var front = require('../front.ejs')();
 test('can create a card', function(assert) {
     assert.plan(1);
     html=createCard(front, 'back');
-    document.body.innerHTML = html;
-    console.log(html);
+    document.body.innerHTML = html + html;
+    document.getElementsByClassName('card')[1].classList.add('flip');
     assert.ok(html);
 });
