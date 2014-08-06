f1635b8658be6c4c6b12554cd11431a31c18f5f0
Jan Bölsche <regular.gonzales@googlemail.com>
2014-08-06 15:40:35 +0200

    added text to the back side

--- test/test.js
+++ test/test.js
@@ -2,10 +2,11 @@ var test = require('prova');
 
 var createCard = require('../cards');
 var front = require('../front.ejs')();
+var back = require('../back.ejs')();
 
 test('can create a card', function(assert) {
     assert.plan(1);
-    html=createCard(front, 'back');
+    html=createCard(front, back);
     document.body.innerHTML = html + html;
     document.getElementsByClassName('card')[1].classList.add('flip');
     assert.ok(html);
